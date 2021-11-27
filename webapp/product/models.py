31051d90d479acad9cac71efc06ddd0b489beb8c
Luis Bardales <hi@luisbardales.com>
2021-11-27 18:27:01 -0500

    add category model

--- webapp/product/models.py
+++ webapp/product/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
-    sku = models.CharField(max_length=160, unique=True)
+class Category(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Product(models.Model):
+    sku = models.CharField(max_length=160, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=15, decimal_places=2)
     created_at = models.DateTimeField(auto_created=True)
     updated_at = models.DateTimeField(auto_now=True)
