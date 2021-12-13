e16df8fef14f1dc0cc4d59b83235ff8a52d490cf
Luis Bardales <hi@luisbardales.com>
2021-12-13 12:36:30 -0500

    update model and migrations

--- webapp/product/models.py
+++ webapp/product/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=200)
+    image_id = models.IntegerField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_created=True)
     updated_at = models.DateTimeField(auto_now=True)
--- webapp/product/views.py
+++ webapp/product/views.py
@@ -4,16 +4,16 @@ from django.utils import timezone
 from .models import Category
 
 def seeders(request):
-    query = Category(title="For Men", description="For Men blah blah", created_at=timezone.now())
+    query = Category(title="For Men", image_id=1, description="For Men blah blah", created_at=timezone.now())
     query.save()
 
-    query = Category(title="For Women", description="For Women blah blah", created_at=timezone.now())
+    query = Category(title="For Women", image_id=2, description="For Women blah blah", created_at=timezone.now())
     query.save()
     
-    query = Category(title="Accessories", description="Accessories blah blah", created_at=timezone.now())
+    query = Category(title="Accessories", image_id=3, description="Accessories blah blah", created_at=timezone.now())
     query.save()
     
-    query = Category(title="Cosmetic", description="Cosmetic blah blah", created_at=timezone.now())
+    query = Category(title="Cosmetic", image_id=4, description="Cosmetic blah blah", created_at=timezone.now())
     query.save()
 
     return HttpResponse("Seeders saved into database")
