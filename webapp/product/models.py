87f783fbf15dbd2d0a3c2660fc91093cd3227fdd
Luis Bardales <hi@luisbardales.com>
2021-12-14 19:08:16 -0500

    customize models

--- webapp/product/models.py
+++ webapp/product/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Category(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=160)
+    slug = models.SlugField(max_length=200, unique=True)
     image_id = models.IntegerField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_created=True)
@@ -12,8 +13,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     sku = models.CharField(max_length=160, unique=True)
+    title = models.CharField(max_length=160)
     slug = models.SlugField(max_length=200, unique=True)
-    title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=15, decimal_places=2)
     created_at = models.DateTimeField(auto_created=True)
--- webapp/product/views.py
+++ webapp/product/views.py
@@ -1,19 +1,77 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Category
+from .models import Product
+
+categories = [
+    {
+        "title": "Smartphones",
+        "slug": "smartphones",
+        "image_id": 1,
+        "description": "Smartphones blah blah",
+        "created_at": timezone.now()
+    },
+    {
+        "title": "Laptops",
+        "slug": "laptops",
+        "image_id": 2,
+        "description": "Laptops blah blah",
+        "created_at": timezone.now()
+    },
+    {
+        "title": "Televisions",
+        "slug": "televisions",
+        "image_id": 3,
+        "description": "Televisions blah blah",
+        "created_at": timezone.now()
+    },
+    {
+        "title": "Watches",
+        "slug": "watches",
+        "image_id": 4,
+        "description": "Watches blah blah",
+        "created_at": timezone.now()
+    }
+]
+
+products = [
+    {
+        "sku": "UGG-BB-PUR-06",
+        "title": "Samsung Galaxy A12 (Blue, 64GB, 4GB RAM)",
+        "slug": "samsung-galaxy-a12-blue-64gb-4gb-ram",
+        "description": "Samsung Galaxy A12 (Blue, 64GB, 4GB RAM)",
+        "price": "500",
+        "created_at": timezone.now()
+    },
+    {
+        "sku": "UGG-BB-PUR-07",
+        "title": "Redmi 8 64GB (Sapphire Blue, 4GB RAM)",
+        "slug": "redmi-8-64gb-sapphire-blue-4gb-ram",
+        "description": "Redmi 8 64GB (Sapphire Blue, 4GB RAM)",
+        "price": "450",
+        "created_at": timezone.now()
+    },
+    {
+        "sku": "UGG-BB-PUR-06",
+        "title": "Samsung Galaxy A12 (Blue, 64GB, 4GB RAM)",
+        "slug": "samsung-galaxy-a12-blue-64gb-4gb-ram",
+        "description": "Samsung Galaxy A12 (Blue, 64GB, 4GB RAM)",
+        "price": "500",
+        "created_at": timezone.now()
+    },
+    {
+        "sku": "UGG-BB-PUR-07",
+        "title": "Redmi 8 64GB (Sapphire Blue, 4GB RAM)",
+        "slug": "redmi-8-64gb-sapphire-blue-4gb-ram",
+        "description": "Redmi 8 64GB (Sapphire Blue, 4GB RAM)",
+        "price": "450",
+        "created_at": timezone.now()
+    }
+]
 
 def seeders(request):
+    """
     query = Category(title="For Men", image_id=1, description="For Men blah blah", created_at=timezone.now())
     query.save()
-
-    query = Category(title="For Women", image_id=2, description="For Women blah blah", created_at=timezone.now())
-    query.save()
-    
-    query = Category(title="Accessories", image_id=3, description="Accessories blah blah", created_at=timezone.now())
-    query.save()
-    
-    query = Category(title="Cosmetic", image_id=4, description="Cosmetic blah blah", created_at=timezone.now())
-    query.save()
-
+    """
     return HttpResponse("Seeders saved into database")
